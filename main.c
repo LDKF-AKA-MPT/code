b4cd8bed39e00b0474e25738baf3c7139ff224ef
GavinNeyman <43770848+GavinNeyman@users.noreply.github.com>
2020-02-19 11:47:17 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,97 @@
-#include <msp430.h>
-int distance = 12;
-int counter = 0;
+#include <msp430.h> 
+int button1Flag = 0;
+int button2Flag = 0;
+/*============================================================================
+*   Title: Initalization of the MSP430
+*
+*   Description:Initialize all values and perefeials that will be used throughout the
+*   program
+*
+*
+*
+*
+*
+*
+*
+============================================================================*/
 
-int main(void)
+void init_msp430(){
+    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+    __bis_SR_register( GIE ); // Let interupts be global
+
+    P1DIR |= BIT0; // Set P1.0 as output
+    P1OUT &= ~BIT0; // Initally led off
+
+    P1REN |= BIT1; // Enable P1.1 internal resistor
+    P1OUT |= BIT1; // Set P1.1 as pull-Up resistor
+    P1IES &= ~BIT1; // P1.1 Lo/Hi edge
+    P1IFG &= ~BIT1; // P1.1 IFG cleared
+    P1IE |= BIT1; // P1.1 interrupt enabled
+
+    P2REN |= BIT1; // Enable P2.1 internal resistor
+    P2OUT |= BIT1; // Set P2.1 as pull-Up resistor
+    P2IES &= ~BIT1; // P2.1 Lo/Hi edge
+    P2IFG &= ~BIT1; // P2.1 IFG cleared
+    P2IE |= BIT1; // P2.1 interrupt enabled
+
+    TA0CCR0 = 512-1;                          // PWM Period
+    TA0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
+    TA0CCR1 = 256;                            // CCR1 PWM duty cycle
+    TA0CTL = TASSEL_2 + MC_1 + TACLR;         // SMCLK, up mode, clear TAR
+
+    TA1CCTL0 = CCIE;                          // CCR0 interrupt enabled
+    TA1CCR0 = 65536-1;
+    TA1CTL = TASSEL_2 + MC_1 + TACLR;
+
+    __bis_SR_register(LPM0_bits+GIE);             // Enter LPM0
+    __no_operation();                         // For debugger
+}
+
+/*===============================================================================
+* main.c
+*
+*
+*
+================================================================================*/
+
+void main (void)
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  P1DIR |= BIT2;                            // P1.2 output
-  P1SEL |= BIT2;                            // P1.2 options select
+    while(1){
+        if (button1Flag){
+            distance -= 1;
+            if (distance < 1){
+                distance = 12;
+            }
+            P1OUT ^= BIT0;
+        }
+        if (button2Flag){
 
-  P1DIR |= BIT0; // Set P1.0 as output
-  P1OUT &= ~BIT0; // Initally led off
+        }
+    }
+}
 
-  P1REN |= BIT1; // Enable P1.1 internal resistor
-  P1OUT |= BIT1; // Set P1.1 as pull-Up resistor
-  P1IES &= ~BIT1; // P1.1 Lo/Hi edge
-  P1IFG &= ~BIT1; // P1.1 IFG cleared
-  P1IE |= BIT1; // P1.1 interrupt enabled
+// P1.1 interrupt
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+    switch( __even_in_range( P1IV, P1IV_P1IFG7 )) {
+    case P1IV_P1IFG1:
+        button1Flag = 1;
+        break;
+    default:   _never_executed();
+    }
+}
 
-  TA0CCR0 = 512-1;                          // PWM Period
-  TA0CCTL1 = OUTMOD_7;                      // CCR1 reset/set
-  TA0CCR1 = 256;                            // CCR1 PWM duty cycle
-  TA0CTL = TASSEL_2 + MC_1 + TACLR;         // SMCLK, up mode, clear TAR
-
-  TA1CCTL0 = CCIE;                          // CCR0 interrupt enabled
-  TA1CCR0 = 65536-1;
-  TA1CTL = TASSEL_2 + MC_1 + TACLR;
-/*  while (1) {
-      scanf("%d", &distance);
-  }*/
-  __bis_SR_register(LPM0_bits+GIE);             // Enter LPM0
-  __no_operation();                         // For debugger
+// P2.1 interrupt
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
+{
+    switch( __even_in_range( P2IV, P2IV_P2IFG7 )) {
+    case P2IV_P2IFG1:
+        button2Flag = 1;
+        break;
+    default:   _never_executed();
+    }
 }
 
 // Timer0 A0 interrupt service routine
@@ -71,20 +133,3 @@ void __attribute__ ((interrupt(TIMER1_A0_VECTOR))) TIMER1_A0_ISR (void)
     }
     counter ++;
 }
-
-#pragma vector=PORT1_VECTOR
-__interrupt void Port_1(void)
-{
-    switch( __even_in_range( P1IV, P1IV_P1IFG7 )) {
-    case P1IV_P1IFG1:
-        distance -= 1;
-        if (distance < 1) {
-            distance = 12;
-        }
-        P1OUT^=BIT0;
-        break;
-    default:   _never_executed();
-    }
-}
-
-
