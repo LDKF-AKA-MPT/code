0899efde85a90bf239f1821cc3fed3fe97b350e0
GavinNeyman <43770848+GavinNeyman@users.noreply.github.com>
2020-02-26 11:49:53 -0500

    Add files via upload

--- lcd.c
+++ lcd.c
@@ -6,6 +6,7 @@
  */
 
 #include <msp430.h>
+#include <string.h>
 
 /*
  * SCL: Serial Clock (pin 14) (p4.3)
@@ -16,37 +17,39 @@
 
 void data_wr(unsigned char data){
     P1OUT &= ~BIT3; //SS = 0;
-    P4OUT &= ~BIT3; //SCL = 0;
     while(!(UCB1IFG&UCTXIFG)); // Wait for TX buffer ready
-    UCB1TXBUF = data;
-    __delay_cycles(100);
+    UCB1TXBUF = data; // Fill and send buffer
+    __delay_cycles(100); // Wait for buffer to send
+    // Pulse clock, Data is sent on rising edge
+    P4OUT &= ~BIT3; //SCL = 0;
+    __delay_cycles(500);
     P4OUT |= BIT3; //SCL = 1;
     P1OUT |= BIT3; //SS = 1;
 }
 
-void str_wr(char* string){
-    while(string){
-        data_wr(string[0]);
-        string++;
+void str_wr(char* data){
+    int i;
+    int strLen = strlen(data);
+    for(i = 0; i < strLen; i++){
+        data_wr(data[i]);
     }
 }
 
 void init_msp430(){
     WDTCTL = WDTPW + WDTHOLD;
 
-    P4SEL |= BIT1 + BIT3; // SPI pins 4.1 (UCB1SIMO), 4.3 (UCB1CLK)
+    P4SEL |= BIT1 + BIT2 + BIT3; // SPI pins 4.1 (UCB1SIMO), 4.2 (UCB1SOMI) 4.3 (UCB1CLK)
     P1DIR |= BIT3; // P1.3 for Slave Select
 
+    UCSCTL2 |= FLLD1 + FLLD0; // Set clock divided by 8
+
     UCB1CTL1 |= UCSWRST; // Reset state machine
-    UCB1CTL0 |= UCMST + UCSYNC + UCCKPL + UCMSB; // UCMST (Master), UCSYNC (SPI), UCCKPL (Clock Polarity), UCMSB (MSB)
+    UCB1CTL0 |= UCMST + UCSYNC + UCCKPL + UCMSB; // UCMST (Master), UCSYNC (SPI), UCCKPL (Clock Polarity), UCMSB (Active State High)
+    UCB1CTL0 &= ~(BIT1 + BIT2); // 3-pin SPI mode for UCSYNC
     UCB1CTL1 |= UCSSEL_2; // SMCLK
     UCB1BR0 = 0x02; // Set baud rate for UCB1
     UCB1BR1 = 0;
-    UCA0MCTL = 0; // No modulation
     UCB1CTL1 &= ~UCSWRST; // Init state machine
-    UCA0IE |= UCRXIE; // Enable RX interrupt
-
-    __delay_cycles(1000); // Wait for slave to init
 }
 
 void clear_lcd(){
@@ -61,10 +64,10 @@ void init_lcd(){
     data_wr(0x42); // Display Off
     data_wr(0xFE); // Command Prompt
     data_wr(0x52); // Set Contrast
-    data_wr(40); // contrast 1 - 50
-    data_wr(0xFE); // Command Prompt
-    data_wr(0x53); // Set Brightness
-    data_wr(15); // backlight 1 - 15
+    data_wr(0x28); // contrast 1 - 50
+    //data_wr(0xFE); // Command Prompt
+    //data_wr(0x53); // Set Brightness
+    //data_wr(0x0F); // backlight 1 - 15
     data_wr(0xFE); // Command Prompt
     data_wr(0x48); // Underline Cursor Off
     data_wr(0xFE); // Command Prompt
@@ -79,11 +82,8 @@ int main(void) {
     init_msp430();
     init_lcd();
 
-    str_wr("Working");
-
-    while(1){
-        // Do Nothing
-    }
+    clear_lcd();
+    str_wr("Hi Todd!");
 }
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
