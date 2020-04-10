8cd189b23e5fad97e019ed9a209156681832ecb9
you35 <46499319+you35@users.noreply.github.com>
2020-04-10 16:54:40 -0400

    Variable type changed to float

--- distance&bearing.c
+++ distance&bearing.c
@@ -1,21 +1,21 @@
 #include <math.h>
 
-double to_radians(double degrees) {
+float to_radians(float degrees) {
     // Convert degrees to radians
-    return degrees/180.0*3.14159265359;
+    return degrees/180.0*Pi;
 }
 
-double calculateDistance(double lat1, double lat2, double deltaLat, double deltaLon){
+float calculateDistance(float lat1, float lat2, float deltaLat, float deltaLon){
     // Calculate distance between current location and destination using Haversine formula
-//    double lat1 = to_radians(currLat);            // move these 4 lines to caller function
-//    double lat2 = to_radians(destLat);
-//    double deltaLat = to_radians(destLat-currLat);
-//    double deltaLon = to_radians(destLon-currLon);
-    double a = pow(sin(deltaLat/2), 2.0) + cos(lat1)*cos(lat2)*pow(sin(deltaLon/2), 2.0);
+//    float lat1 = to_radians(currLat);            // move these 4 lines to caller function
+//    float lat2 = to_radians(destLat);
+//    float deltaLat = to_radians(destLat-currLat);
+//    float deltaLon = to_radians(destLon-currLon);
+    float a = pow(sin(deltaLat/2), 2.0) + cos(lat1)*cos(lat2)*pow(sin(deltaLon/2), 2.0);
     return 2*atan2(sqrt(a), sqrt(1-a))*6371000.0;
 }
 
-double calculateBearing(double lat1, double lat2, double deltaLon) {
+float calculateBearing(float lat1, float lat2, float deltaLon) {
     // Calculate bearing, result is radians from north to east
     // Remember to convert returned value to degrees in caller function
     return atan2(sin(deltaLon)*cos(lat2), cos(lat1)*sin(lat2)-sin(lat1)*cos(lat2)*cos(deltaLon));
